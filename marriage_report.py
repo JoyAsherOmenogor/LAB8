504539b590cd9e499f495af1e468094895afdb57
JoyAsherOmenogor <133912827+JoyAsherOmenogor@users.noreply.github.com>
2023-07-28 23:29:34 -0400

    Update marriage_report.py

--- marriage_report.py
+++ marriage_report.py
@@ -9,6 +9,7 @@ Usage:
 import os
 import sqlite3
 from create_relationships import db_path, script_dir
+import pandas as pd
 
 def main():
     # Query DB for list of married couples
@@ -25,21 +26,12 @@ def get_married_couples():
         list: (name1, name2, start_date) of married couples 
     """
     # TODO: Function body
-    # Hint: See example code in lab instructions entitled "Get a List of Relationships"
     con = sqlite3.connect(db_path)
-    return
-
-def save_married_couples_csv(married_couples, csv_path):
-    """Saves list of married couples to a CSV file, including both people's 
-    names and their wedding anniversary date  
-
-    Args:
-        married_couples (list): (name1, name2, start_date) of married couples
-        csv_path (str): Path of CSV file
+    cur = con.cursor()
+    all_relationships_query = """
+        SELECT person1.name, person2.name, start_date FROM relationships
+        JOIN people person1 ON person1_id = person1.id
+        JOIN people person2 ON person2_id = person2.id
+        WHERE type = 'spouse';
     """
-    # TODO: Function body
-    # Hint: We did this in Lab 7.
-    return
-
-if __name__ == '__main__':
-   main()
+    cur.execute(all_relationships_query)
